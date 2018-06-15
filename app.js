5ccc1704e8a10cd8d675172aa58c6112fe660e61
zeezzad <panon508@gmail.com>
2018-06-15 11:40:04 +0700

    V.2

--- app.js
+++ app.js
@@ -3,16 +3,33 @@ var app = express();
 var multer = require('multer');
 var bodyParser = require('body-parser');
 var fs = require('fs');
-
+var mysql = require('mysql');
 var upload = multer({ dest: 'tmp/' });
 
 app.use(express.static('public'));
 var urlcodedParser = bodyParser.urlencoded({ extended: false });
 
+var con = mysql.createConnection({
+host : "localhost",
+user : "root",
+password : "12345678",
+database : "jobreq"
+});
 
+//index
 app.get('/index.htm', function (req, res) {
     res.sendFile(__dirname + "/" + "index.htm");
+
+    con.connect(function (err) {
+        if(err) throw err;
+        con.query("",function (err,result,fields) {
+            if(err) throw err;
+            console.log(result);
+        })
+    })
+    
 })
+
 
 //get process
 app.get('/process_get', function (req, res) {
@@ -49,4 +66,4 @@ var server = app.listen(8081, function () {
 
     console.log("Example app listen  att http://%s:%s", host, port);
     console.log("localhost:8081");
-});
+})
